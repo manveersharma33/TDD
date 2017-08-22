ff751a481d64b638c133ba8ced76344a3a859bf6
manveersharma33 <31258634+manveersharma33@users.noreply.github.com>
2017-08-22 22:55:11 +0100

    Add files via upload
    cs file

---
+++ PrimeFactors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public static List<int> GetPrimes(int number)
+        {
+            var primes = new List<int>();
+
+            for(int divisor = 2; divisor<=number; divisor++) 
+             {
+               while(number % divisor == 0) 
+               {
+                    primes.Add(divisor);
+                    number = number / divisor;
+                }
+            }
+            return primes;
+        }
+
+
+    }
+}
